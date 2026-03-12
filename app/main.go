6011322d9a4df90753880ea5dba6d4e929ba3376
Charles Knight <charlesknight@charless-mbp.lan>
2026-03-12 13:22:43 -0500

    changing hw app to test ci/cd autodeploy

--- app/main.go
+++ app/main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, World!")
+		fmt.Fprintln(w, "Hello, World! rev b")
 	})
 
 	server := &http.Server{
